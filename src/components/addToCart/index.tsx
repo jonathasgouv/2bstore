6b4ce4d0ce8f210c7443fcf851e68a8d576ec0b1
Jõnathas Gouveia <jonathas_gouv@hotmail.com>
2023-10-21 11:59:46 -0300

    adds incrementing adding + fix minicart body max height

--- src/components/addToCart/index.tsx
+++ src/components/addToCart/index.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from 'react'
 import Loading from '../loading';
 import use2BStore from '../../stores/2bStore';
-import { addSkusToCart } from '../../helpers/vtex';
+import { addSkusToCart, updateSkusOnCart } from '../../helpers/vtex';
 
 import './addToCart.scss'
 
@@ -15,6 +15,7 @@ interface IAddToCart {
 
 const AddToCart: FC<IAddToCart> = ({ sku, quantity = 1, label = 'Comprar', seller = '1', orderFormId }) => {
     const [isLoading, setIsLoading] = useState(false)
+    const orderForm = use2BStore(state => state.orderForm)
     const { updateOrderForm, toggleMiniCart } = use2BStore((state) => {
         return {
             updateOrderForm: state.updateOrderForm,
@@ -25,6 +26,28 @@ const AddToCart: FC<IAddToCart> = ({ sku, quantity = 1, label = 'Comprar', selle
     const addSkuToCart = async () => {
         setIsLoading(true)
 
+        const isInCart = orderForm?.items.find(item => item.id === sku)
+        
+        // If item is already on cart, sums the current quantity with the added
+        if (isInCart) {
+            const skuIndex = orderForm?.items.findIndex(item => item.id === isInCart.id)
+
+            const item = [{
+                index: skuIndex as number,
+                quantity: (isInCart?.quantity as number) + quantity
+            }]
+
+            const newOrderForm = await updateSkusOnCart(item, orderFormId)
+
+            if (newOrderForm) updateOrderForm(newOrderForm)
+
+            setIsLoading(false)
+            toggleMiniCart()
+
+            return
+        }
+
+        // If item is not on cart just adds it
         const item = [{
             id: sku,
             quantity,
@@ -41,8 +64,8 @@ const AddToCart: FC<IAddToCart> = ({ sku, quantity = 1, label = 'Comprar', selle
 
     return (
         <button className="productAddToCart" onClick={addSkuToCart}>
-            <span style={{visibility: isLoading ? 'visible' : 'hidden'}}><Loading size='12px' /></span>
-            <span style={{visibility: isLoading ? 'hidden' : 'visible'}}>{label}</span>
+            <span style={{ visibility: isLoading ? 'visible' : 'hidden' }}><Loading size='12px' /></span>
+            <span style={{ visibility: isLoading ? 'hidden' : 'visible' }}>{label}</span>
         </button>
     )
 }
